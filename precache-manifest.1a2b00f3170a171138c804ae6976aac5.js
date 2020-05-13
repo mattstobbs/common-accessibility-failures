a8a907e49e25418f9efed68d47e8a86893614786
mattstobbs <mattstobbs@hotmail.co.uk>
2020-05-13 14:00:06 +0100

    Updates

---
+++ precache-manifest.1a2b00f3170a171138c804ae6976aac5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97d0e8f5430ee4847972866568ef916",
+    "url": "/common-accessibility-failures/index.html"
+  },
+  {
+    "revision": "c6728b0232ad1a93eacf",
+    "url": "/common-accessibility-failures/static/css/main.c93807dd.chunk.css"
+  },
+  {
+    "revision": "cf2a6d3aab8c59b32a31",
+    "url": "/common-accessibility-failures/static/js/2.32024f0f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/common-accessibility-failures/static/js/2.32024f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6728b0232ad1a93eacf",
+    "url": "/common-accessibility-failures/static/js/main.a2bcb552.chunk.js"
+  },
+  {
+    "revision": "8f0b7f0a15921091917e",
+    "url": "/common-accessibility-failures/static/js/runtime-main.de2cd70d.js"
+  }
+]);
