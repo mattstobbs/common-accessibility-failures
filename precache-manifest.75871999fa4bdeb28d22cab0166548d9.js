f03b656310e5aae74e0108723af2c5bebcdcecfd
mattstobbs <mattstobbs@hotmail.co.uk>
2020-05-15 13:27:11 +0100

    Updates

--- precache-manifest.75871999fa4bdeb28d22cab0166548d9.js
+++ precache-manifest.75871999fa4bdeb28d22cab0166548d9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b396e8fee85446964c260c5fa6b9046",
+    "revision": "d88fa147276e42a470a4da2909ee3fb3",
     "url": "/common-accessibility-failures/index.html"
   },
   {
-    "revision": "bae406bd548b772b8ac3",
+    "revision": "54331080513386ea4c9d",
     "url": "/common-accessibility-failures/static/css/2.e4477242.chunk.css"
   },
   {
-    "revision": "fc68e06ba60740bb26e6",
+    "revision": "1518d0360861d9103be3",
     "url": "/common-accessibility-failures/static/css/main.5c771b38.chunk.css"
   },
   {
-    "revision": "bae406bd548b772b8ac3",
-    "url": "/common-accessibility-failures/static/js/2.3bf15b38.chunk.js"
+    "revision": "54331080513386ea4c9d",
+    "url": "/common-accessibility-failures/static/js/2.6a2ac036.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/common-accessibility-failures/static/js/2.3bf15b38.chunk.js.LICENSE.txt"
+    "url": "/common-accessibility-failures/static/js/2.6a2ac036.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fc68e06ba60740bb26e6",
-    "url": "/common-accessibility-failures/static/js/main.f79bdd18.chunk.js"
+    "revision": "1518d0360861d9103be3",
+    "url": "/common-accessibility-failures/static/js/main.df87916b.chunk.js"
   },
   {
     "revision": "8f0b7f0a15921091917e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/common-accessibility-failures/precache-manifest.afc1879a99ceecca3ffe094ba9ab55be.js"
+  "/common-accessibility-failures/precache-manifest.75871999fa4bdeb28d22cab0166548d9.js"
 );
 
 self.addEventListener('message', (event) => {
